fbf588a27e4f28ccefc26357a616265e7eaabd1c
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2024-10-12 02:47:08 +0600

    Beecrowd problem 2413
    Accepted

---
+++ 2413.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int numOfPeopleVisitingThirdLink;
+    scanf("%d", &numOfPeopleVisitingThirdLink);
+    
+    // numOfPeopleVisitingFirstLink
+    printf("%d\n", 4 * numOfPeopleVisitingThirdLink);
+}
